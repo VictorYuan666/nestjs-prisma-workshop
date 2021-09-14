65cfcd74955347b726f09f7cbc36e11fcf48446e
袁小威 <yuanxiaowei@u51.com>
2021-09-14 18:31:40 +0800

    feat: init

---
+++ dist/products/products.controller.js
@@ -0,0 +1,99 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductsController = void 0;
+const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
+const swagger_2 = require("@nestjs/swagger");
+const products_service_1 = require("./products.service");
+const create_product_dto_1 = require("./dto/create-product.dto");
+const update_product_dto_1 = require("./dto/update-product.dto");
+const product_entity_1 = require("./entities/product.entity");
+let ProductsController = class ProductsController {
+    constructor(productsService) {
+        this.productsService = productsService;
+    }
+    create(createProductDto) {
+        return this.productsService.create(createProductDto);
+    }
+    findAll() {
+        return this.productsService.findAll();
+    }
+    async findOne(id) {
+        return new product_entity_1.ProductEntity(await this.productsService.findOne(+id));
+    }
+    update(id, updateProductDto) {
+        return this.productsService.update(+id, updateProductDto);
+    }
+    remove(id) {
+        return this.productsService.remove(+id);
+    }
+    findDrafts() {
+        return this.productsService.findDrafts();
+    }
+};
+__decorate([
+    (0, common_1.Post)(),
+    (0, swagger_2.ApiCreatedResponse)({ type: product_entity_1.ProductEntity }),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [create_product_dto_1.CreateProductDto]),
+    __metadata("design:returntype", void 0)
+], ProductsController.prototype, "create", null);
+__decorate([
+    (0, common_1.Get)(),
+    (0, swagger_2.ApiOkResponse)({ type: [product_entity_1.ProductEntity] }),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], ProductsController.prototype, "findAll", null);
+__decorate([
+    (0, common_1.Get)(':id'),
+    (0, swagger_2.ApiOkResponse)({ type: product_entity_1.ProductEntity }),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], ProductsController.prototype, "findOne", null);
+__decorate([
+    (0, common_1.Patch)(':id'),
+    (0, swagger_2.ApiOkResponse)({ type: product_entity_1.ProductEntity }),
+    __param(0, (0, common_1.Param)('id')),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, update_product_dto_1.UpdateProductDto]),
+    __metadata("design:returntype", void 0)
+], ProductsController.prototype, "update", null);
+__decorate([
+    (0, common_1.Delete)(':id'),
+    (0, swagger_2.ApiOkResponse)({ type: product_entity_1.ProductEntity }),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], ProductsController.prototype, "remove", null);
+__decorate([
+    (0, common_1.Get)('drafts'),
+    (0, swagger_2.ApiOkResponse)({ type: [product_entity_1.ProductEntity] }),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], ProductsController.prototype, "findDrafts", null);
+ProductsController = __decorate([
+    (0, common_1.Controller)('products'),
+    (0, swagger_1.ApiTags)('products'),
+    __metadata("design:paramtypes", [products_service_1.ProductsService])
+], ProductsController);
+exports.ProductsController = ProductsController;
+//# sourceMappingURL=products.controller.js.map
---
+++ src/products/products.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductEntity } from './entities/product.entity';
+
+@Controller('products')
+@ApiTags('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @ApiCreatedResponse({ type: ProductEntity })
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  @ApiOkResponse({ type: [ProductEntity] })
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: ProductEntity })
+  async findOne(@Param('id') id: string) {
+    return new ProductEntity(await this.productsService.findOne(+id));
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ type: ProductEntity })
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: ProductEntity })
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+
+  @Get('drafts')
+  @ApiOkResponse({ type: [ProductEntity] })
+  findDrafts() {
+    return this.productsService.findDrafts();
+  }
+}
